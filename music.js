940391e3a30dc321811466bfa99a81e4083a7734
Ron Herrema <rherrema@gmail.com>
2022-04-29 19:45:55 +0100

    first commit

---
+++ music.js
@@ -0,0 +1,51 @@
+// const synth = new Tone.Synth().toDestination()
+
+// import { Panner3D } from "tone"
+
+// synth.triggerAttackRelease("C4", "8n")
+const notes = ["C2", "C3", "E3", "F3", "G3", "C4", "E4", "D4"]
+
+let synth
+
+let panner
+
+panner = new Tone.Panner3D(-2, 5, -7)
+
+synth = new Tone.PolySynth({
+  volume: -10,
+  envelope: {
+    attack: 0.1,
+    decay: 0,
+    sustain: 0.05,
+    release: 0,
+  },
+})
+
+synth.set({
+  oscillator: {
+    type: "sine",
+  },
+})
+
+synth.chain(panner, Tone.Destination())
+
+var pattern = new Tone.Pattern(
+  (time, note) => synth.triggerAttackRelease(note, "16n"),
+  Array(12)
+    .fill()
+    .map(() => notes[Math.floor(Math.random() * notes.length)]),
+  "upDown"
+)
+
+pattern.loop = false
+pattern.interval = "12n"
+
+document.getElementById("play-button").addEventListener("click", function () {
+  if (Tone.Transport.state !== "started") {
+    Tone.start()
+    Tone.Transport.start()
+    pattern.start(0)
+  } else {
+    Tone.Transport.stop()
+  }
+})
